14d822bc00d09faf85466546b6f413116707e15f
StoneFree2011 <greendayarseniy@gmail.com>
2023-12-06 22:50:05 +0300

    Expense.h, ExpenseInputScreen.h, ExpenseList.h

--- Baker-street/ExpenseList.h
+++ Baker-street/ExpenseList.h
@@ -3,5 +3,18 @@
 
 #include "Expense.h"
 
+//Класс записей о затратах
+class ExpenseList
+{
+private:
+    vector<Expense*> vectPtrsExpenses; //вектор указателей на расходы
+    vector<Expense*>::iterator iter;
+public:
+    ~ExpenseList();
+    void insertExp(Expense*);
+    void display();
+    float displaySummary(); // нужно для годового отчета
+};
+
 #endif // EXPENSELIST
 
