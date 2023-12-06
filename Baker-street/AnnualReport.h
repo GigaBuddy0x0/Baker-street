737c4cf8ae819cf10bcfb2ae3d53c0a8eedb4be9
Leonardo <alexrokorianskii@gmail.com>
2023-12-06 20:50:46 +0300

    update UserInterface.h and AnnualReport.h

--- Baker-street/AnnualReport.h
+++ Baker-street/AnnualReport.h
@@ -1,5 +1,16 @@
 #ifndef ANNUALREPORT
 #define ANNUALREPORT
-
+//////////////////класс AnnualReport///////////////////////
+//Класс годового отчета
+class AnnualReport
+{
+private:
+ProfitList* ptrPL; // записи доходов
+ExpenseList* ptrEL; // записи расходов
+float expenses, profit; // суммы доходов и расходов
+public:
+AnnualReport(ProfitList*, ExpenseList*);
+void display(); // отображение годового отчета
+};
 #endif // ANNUALREPORT
 
--- Baker-street/UserInterface.h
+++ Baker-street/UserInterface.h
@@ -1,5 +1,24 @@
 #ifndef USERINTERFACE
 #define USERINTERFACE
-
+//////////////////Класс UserInterface//////////////////////
+//Главный класс для запуска приложения:
+//этот класс определяет взаимодействие юзера с программой.
+class UserInterface
+{
+private:
+ProduceList* ptrProduceList;
+ProduceInputScreen* ptrProduceInputScreen;
+ProfitList* ptrProfitList;
+ProfitInputScreen* ptrProfitInputScreen;
+ExpenseList* ptrExpenseList;
+ExpenseInputScreen* ptrExpenseInputScreen;
+AnnualReport* ptrAnnualReport;
+char ch;
+public:
+UserInterface();
+~UserInterface();
+void interact();
+}; // конец класса userInterfac
+//////////////////конец файла landlord.h//////////////////
 #endif // USERINTERFACE
 
