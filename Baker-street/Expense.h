7000be15ee253587f6a7bb1d09052cebdddef231
StoneFree2011 <greendayarseniy@gmail.com>
2023-12-10 17:48:11 +0300

    Добавлены get-теры в заголовочный файл ExpenseList.h, расширены поля ExpenseInputScreen.h

--- Baker-street/Expense.h
+++ Baker-street/Expense.h
@@ -8,17 +8,18 @@ class Expense
 {
 private:
     string category; // категория расходов (газ, свет, ремонт и тд)
-    string expense_item; //название расхода
-    string month; // месяц уплаты расходов
+    string name; //название расхода
+    string date; // дата уплаты расходов
     float amount; // сколько платим
 public:
-    Expense(string c, string e, string m, float a):
-        category(c), expense_item(e), month(m),amount(a)
+    Expense(string c, string n, string d, float a):
+        category(c), name(n), date(d), amount(a)
     {}
     ~Expense();
+    //геттеры:
     string GetCategory();
-    string GetExpense_item();
-    string GetMonth();
+    string GetName();
+    string GetDate();
     float GetAmount();
 };
 
