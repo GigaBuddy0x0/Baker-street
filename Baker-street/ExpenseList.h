dff7c801382d0b183465d57e24bb61162767f115
StoneFree2011 <151025181+StoneFree2011@users.noreply.github.com>
2023-12-10 18:02:33 +0300

    Добавлены get-теры в заголовочный файл ExpenseList.h, расширены поля ExpenseInputScreen.h. Подправлены комментарии

--- Baker-street/ExpenseList.h
+++ Baker-street/ExpenseList.h
@@ -11,9 +11,9 @@ private:
     vector<Expense*>::iterator iter;
 public:
     ~ExpenseList();
-    void InsertExpense(Expense*);
-    float DisplaySummExpenses();
-    void DisplayExp();
+    void InsertExpense(Expense*); //внести расход
+    float DisplaySummExpenses(); //вывести сумму всех расходов
+    void DisplayExp(); //вывести строку расходов
     //геттеры:
     string GetCategoryExpense(string);
     //string GetNameExpense(string);
