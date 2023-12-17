b0e3a73336f93f8d57b08515019ab6c3e59c0549
Alex <Alexsmirniy11@gmail.com>
2023-12-18 01:16:35 +0300

    Полностью описана реализация класса UserInterface. Вернул файлы globalmetods ( ;( )

--- Baker-street/ProfitList.h
+++ Baker-street/ProfitList.h
@@ -10,11 +10,11 @@ private:
     list <Profit*>::iterator iter; //итератор
 public:
     ~ProfitList(); // деструктор (удаление дохода из списка)
-    void insertProfit(Profit*); // добавить доход в список
-    int GetQuantityProfit(string);
-    double GetGlobalcostProfit(string);
-    string GetDateProfit(string);
-    void DisplaySummProfits(); // вывод списка доходов
+    void InsertProfit(Profit*); // добавить доход в список
+    //int GetQuantityProfit(string);
+    //double GetGlobalcostProfit(string);
+    //string GetDateProfit(string);
+    float DisplaySummProfits(); // вывод списка доходов
     void DisplayProf();
 };
 #endif // PROFITLIST
